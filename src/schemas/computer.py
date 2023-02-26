e47dbde0347e46286ed141decdf9b80d63b0d7b5
lergen <hdl-mebel@mail.ru>
2023-02-26 18:50:01 +0500

    docker

---
+++ src/schemas/computer.py
@@ -0,0 +1,77 @@
+'''
+Файл не используется. Добавил для образца. Взято:
+https://github.com/canyoupleasecreateanaccount/pytestLessonsCode/blob/main/src/schemas/computer.py
+'''
+from pydantic import BaseModel, EmailStr, validator
+from pydantic.types import PastDate, FutureDate, List, PaymentCardNumber
+from pydantic.networks import IPv4Address, IPv6Address
+
+from src.schemas.physical import Physical
+
+"""
+Именно в этом файле можно поиграться с уже готовой моделью и примером
+тестового объекта для неё (Human).
+That file gives to you possibility to play with ready to use model (Human).
+"""
+
+from src.enums.user_enums import Statuses
+
+
+class Owners(BaseModel):
+    name: str
+    card_number: PaymentCardNumber
+    email: EmailStr
+
+
+class DetailedInfo(BaseModel):
+    physical: Physical
+    owners: List[Owners]
+
+
+class Computer(BaseModel):
+    id: int
+    status: Statuses
+    activated_at: PastDate
+    expiration_at: FutureDate
+    host_v4: IPv4Address
+    host_v6: IPv6Address
+    detailed_info: DetailedInfo
+
+
+class Human(BaseModel):
+    name: str
+    last_name: str
+    surname: str = None
+    is_hide: bool
+
+    @validator('is_hide')
+    def validate_surname_showing(cls, hide_value, values):
+        """
+        Пример валидатора, который используется для проверки значения в поле
+        is_hide.
+        Example of validator that we use for checking is_hide field.
+        """
+        if hide_value is False and values.get('surname') is None:
+            raise ValueError('Surname should be presented')
+        return hide_value
+
+
+human = Human.parse_obj({
+    "name": "Andrii",
+    "last_name": "Shevchenko",
+    "is_hide": True
+})
+
+
+class Inventory(BaseModel):
+    sold: int
+    string: int
+    unavailable: int
+    pending: int
+    available: int
+    not_available: int
+    status01: int
+    status: int
+
+obj = Inventory()
+obj.schema_json()
--- tests/test_api.py
+++ tests/test_api.py
@@ -19,7 +19,7 @@ from config import surl
      )
 ] )
 # @pytest.mark.parametrize(params['login'],[('ho', '123123', 'вася')])
-
+@pytest.mark.production
 def test_api(url, method, params, control_response, response_fixture):
     ''' API ЯндексСамокат. Создание нового курьера. Структура ответа при успешном создании.'''
     # print(response_fixture.response_json)
@@ -29,8 +29,8 @@ obj = BuilderJson()
 # сразу создать структуру и добавить к url ручку
 # obj.reset().add('login').add('password').add('firstname')
 # затем через parametrize  передавать данные одного КЭ и структуру ответа для сравнения
-obj.add(['key1','key2','key3','key4'], 123)
-@pytest.mark.last
+obj.add(['key1','key2','key3','key4'], 123) # пример создания вложенной структуры
+@pytest.mark.development
 @pytest.mark.parametrize('value', ('Vasa', 'Вася', '1234', '@#%$@'))
 @pytest.mark.parametrize('key', ('first_name', 'last_name', 'age'))
 def test_create_courier(key, value): # вызвать response_fixture
